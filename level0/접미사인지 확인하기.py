f5173a5024240a7861d62cd57b0ca9272eb17e1f
신현식 <hunsik777@gmail.com>
2025-11-14 15:11:06 +0900

    54

---
+++ level0/접미사인지 확인하기.py
@@ -0,0 +1,8 @@
+def solution(my_string, is_suffix):
+    answer = 0
+    answer_list = []
+
+    for i in range(len(my_string)):
+        answer_list.append(my_string[i:])
+
+    return 1 if is_suffix in answer_list else 0
