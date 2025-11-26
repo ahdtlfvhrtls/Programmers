9b57ef0f2804a6534e82dba78627c41df6d5c7ef
신현식 <hunsik777@gmail.com>
2025-11-26 15:40:19 +0900

    106

--- level0/n보다 커질 때까지 더하기.py
+++ level0/n보다 커질 때까지 더하기.py
@@ -2,3 +2,7 @@ def solution(numbers, n):
     answer = 0
     for i in range(len(numbers)):
         if answer <= n:
+            answer += numbers[i]
+        else:
+            break
+    return answer
