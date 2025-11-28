5895afb12a6271e231757121fe7f9024930bf258
신현식 <hunsik777@gmail.com>
2025-11-28 14:51:57 +0900

    118

--- level0/조건에 맞게 수열 변환하기 2.py
+++ level0/조건에 맞게 수열 변환하기 2.py
@@ -1,3 +1,8 @@
 def solution(arr):
     x = 0
     while(True):
+        arrx = []
+        for i in arr:
+            if i >= 50 and i % 2 == 0:
+                arrx.append(i//2)
+            elif i < 50 and i % 2 != 0:
