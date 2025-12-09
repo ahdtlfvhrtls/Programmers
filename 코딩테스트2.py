f4f3b71686082c7dd30a05f603153aa80a635391
신현식 <hunsik777@gmail.com>
2025-12-09 15:12:45 +0900

    173

--- 코딩테스트2.py
+++ 코딩테스트2.py
@@ -1,7 +1,6 @@
 import pandas as pd
 
 df = pd.read_csv('D:/프로그래머스/videos.csv')
-# 최대 duration 찾기
 max_duration = df['duration'].max()
 
 # 최대 duration을 가진 모든 영상 필터링
@@ -10,4 +9,3 @@ longest_videos = df[df['duration'] == max_duration]
 print("최대 duration:", max_duration)
 print("해당 duration 영상 개수:", len(longest_videos))
 print(longest_videos)
-
