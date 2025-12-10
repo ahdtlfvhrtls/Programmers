cfcc35cfbc4e33851d1aa49567528c0fbfbe3aba
신현식 <hunsik777@gmail.com>
2025-12-10 15:51:50 +0900

    178

--- 코딩테스트2.py
+++ 코딩테스트2.py
@@ -2,4 +2,4 @@ import pandas as pd
 
 df = pd.read_csv('D:/프로그래머스/videos.csv')
 df['total_user_combined'] = df['total_user_time_spent_in_mins'] + df['user_time_spent_versionB_in_mins']
-
+df['total_ads_combined'] = df['total_ads_watched_in_mins'] + df['ads_watched_vesionB_in_mins']
