69bf537b1b00f6fc1b9f09e95421f24afab5ac04
신현식 <hunsik777@gmail.com>
2025-12-11 13:27:42 +0900

    195

--- 코딩테스트2.py
+++ 코딩테스트2.py
@@ -3,3 +3,5 @@ import pandas as pd
 df = pd.read_csv('D:/프로그래머스/videos.csv')
 df_valid = df[df['date'] != '-'].copy()
 df_valid['date'] = pd.to_datetime(df_valid['date'])
+
+daily = df_valid.groupby('date').agg(
