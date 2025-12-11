1195dc3a52be60612c4f2d3c4d1e6ddbf8eb37c7
신현식 <hunsik777@gmail.com>
2025-12-11 13:26:51 +0900

    191

--- 코딩테스트2.py
+++ 코딩테스트2.py
@@ -1,13 +1,4 @@
 import pandas as pd
 
 df = pd.read_csv('D:/프로그래머스/videos.csv')
-df['total_user_combined'] = df['total_user_time_spent_in_mins'] + df['user_time_spent_versionB_in_mins']
-df['total_ads_combined'] = df['total_ads_watched_in_mins'] + df['ads_watched_vesionB_in_mins']
-
-target = df[df['time_slot'].isin(['06:00-11:59','12:00-17:59'])]
-
-total_user = target['total_user_combined'].sum()
-total_ads = target['total_ads_combined'].sum()
-ratio = total_ads / total_user
-
-print(ratio)
+df_valid = df[df['date'] != '-'].copy()
