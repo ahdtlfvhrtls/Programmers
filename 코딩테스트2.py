aecab23f97590620934a19a5a724712cddb940b3
신현식 <hunsik777@gmail.com>
2025-12-17 14:00:27 +0900

    216

--- 코딩테스트2.py
+++ 코딩테스트2.py
@@ -14,3 +14,4 @@ daily = df_valid.groupby('date').agg(
 daily['ratio_A'] = daily['clicks_A'] / daily['time_A']
 daily['ratio_B'] = daily['clicks_B'] / daily['time_B']
 
+daily_compare = daily[daily['ratio_A'] > daily['ratio_B']]
