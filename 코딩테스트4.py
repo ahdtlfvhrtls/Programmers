8024b8ce7f99ef9160e09422e2b2042d45671c77
신현식 <hunsik777@gmail.com>
2026-01-06 16:55:31 +0900

    319

--- 코딩테스트4.py
+++ 코딩테스트4.py
@@ -9,7 +9,7 @@ df_valid['ads_watched_combined'] = (
     df_valid['ads_watched_vesionB_in_mins']
 )
 df_valid['clicks_combined'] = (
-    
+    df_valid['ads_clicked'] +
 
 grouped = df_valid.groupby('date').agg(
     total_ads_watched=('ads_watched_combined', 'sum'),
