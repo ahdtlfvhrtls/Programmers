21478c09d897d920c245f9f93dabe1075b3facbb
신현식 <hunsik777@gmail.com>
2026-01-22 03:52:00 +0900

    403

--- 주(ISO Week 기준) 별 평균.py
+++ 주(ISO Week 기준) 별 평균.py
@@ -16,4 +16,4 @@ df_valid['clicks_B'] = df_valid['ads_clicked_versionB']
 df_valid['week'] = df_valid['date'].dt.isocalendar().week
 
 # 주별 평균
-df_valid['week'] = df_valid['date'].dt.isocalendar().week
+weekly_group = df_valid.groupby('week').agg(
