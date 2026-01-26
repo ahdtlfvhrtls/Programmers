88544b35644fd861fc701772dbaf8cfb6a83d2bb
신현식 <hunsik777@gmail.com>
2026-01-26 17:27:06 +0900

    430

--- 코딩테스트2.py
+++ 코딩테스트2.py
@@ -16,7 +16,7 @@ daily['ratio_B'] = daily['clicks_B'] / daily['time_B']
 
 daily_compare = daily[daily['ratio_A'] > daily['ratio_B']]
 
-
+df['total_user_combined'] = df['total_user_time_spent_in_mins'] + df['user_time_spent_versionB_in_mins']
 
 print("A 비율이 더 높은 날짜 수:", len(daily_compare))
 print("전체 날짜 수:", len(daily))
