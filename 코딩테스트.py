ae891d16882591fba2c8c4c5c53b1b623e66cb59
신현식 <hunsik777@gmail.com>
2026-02-04 17:30:07 +0900

    468

--- 코딩테스트.py
+++ 코딩테스트.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 df = pd.read_csv('D:/프로그래머스/videos.csv')
 
+
 df_valid = df[df['date'] != '-'].copy()
 
 df_valid['total_clicks_combined'] = df_valid['ads_clicked'] + df_valid['ads_clicked_versionB']
