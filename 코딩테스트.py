e7e72e32d378869990559b24a22e799b6e43a65b
신현식 <hunsik777@gmail.com>
2026-02-05 22:48:53 +0900

    474

--- 코딩테스트.py
+++ 코딩테스트.py
@@ -8,6 +8,7 @@ df_valid = df[df['date'] != '-'].copy()
 # 광고 클릭 합산 A + B 
 df_valid['total_clicks_combined'] = df_valid['ads_clicked'] + df_valid['ads_clicked_versionB']
 
+
 grouped_clicks = df_valid.groupby('date').agg(
     total_clicks=('total_clicks_combined','sum')
 )
