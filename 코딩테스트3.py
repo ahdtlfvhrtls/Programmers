425bc6c62cb9a4b4a87f77ee197be6b47b149693
신현식 <hunsik777@gmail.com>
2026-02-10 04:34:52 +0900

    500

--- 코딩테스트3.py
+++ 코딩테스트3.py
@@ -26,6 +26,6 @@ grouped = df_valid.groupby('date').agg(
 grouped = grouped[grouped['total_ads_watched'] > 0].copy()
 
 # 클릭 비율 계산
-
+grouped['click_ratio'] = grouped['total_clicks'] / grouped['total_ads_watched']
 result = grouped.sort_values('click_ratio')
 print(result.head(1))
