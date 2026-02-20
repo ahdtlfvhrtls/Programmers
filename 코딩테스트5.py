599e29a62d8eac6b9c26040c00dd51067d652c6b
신현식 <hunsik777@gmail.com>
2026-02-20 15:41:22 +0900

    542

--- 코딩테스트5.py
+++ 코딩테스트5.py
@@ -4,5 +4,5 @@ df = pd.read_csv('data2.csv')
 
 # group by website
 grouped = df.groupby('website')['contact_mail'].apply(
-    lambda x: x.isna().all()
+    lambda x: x.isna().all() 
 df2 = df.dropna()
