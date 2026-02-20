fe041735cbee3f44ceee57aae702c07d2a7b6872
신현식 <hunsik777@gmail.com>
2026-02-20 15:41:39 +0900

    544

--- 코딩테스트5.py
+++ 코딩테스트5.py
@@ -5,4 +5,5 @@ df = pd.read_csv('data2.csv')
 # group by website
 grouped = df.groupby('website')['contact_mail'].apply(
     lambda x: x.isna().all() # 해당 웹사이트가 등장한 모든 row에서 mail이 NaN이면 True
+    
 df2 = df.dropna()
