b3fc99be5d4b8c3053e8c8fdb30fa565974e9511
신현식 <hunsik777@gmail.com>
2026-02-27 21:27:38 +0900

    579

--- 코딩테스트5.py
+++ 코딩테스트5.py
@@ -22,7 +22,7 @@ df.groupby('website')['contact_mail'] \
 
 len(df[df['contact_mail'].notna() & df['website'].notna()])
 
-
+df['domain'] = df['contact_mail'].str.split('@').str[-1]
 
 df2 = df.dropna()
 df2['contact_mail'].isna().sum()
