031a8cf949a8f9084cf65bf9d2db3922a5088ba7
신현식 <hunsik777@gmail.com>
2026-03-12 16:17:38 +0900

    625

--- 코딩테스트5.py
+++ 코딩테스트5.py
@@ -37,6 +37,7 @@ missing_mail = df[df['contact_mail'].isna() & df['website'].notna()]
 unique_websites = missing_mail['website'].nunique()
 print(unique_websites)
 
+
 df2 = df.dropna()
 
 df2['mail_domain'] = df2['contact_mail'].str.split('@').str[-1]
