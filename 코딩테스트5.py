19c415f0f1689f0567542f4743d82496415e9eb3
신현식 <hunsik777@gmail.com>
2026-03-30 13:22:07 +0900

    690

--- 코딩테스트5.py
+++ 코딩테스트5.py
@@ -49,7 +49,7 @@ df2 = df.dropna()
 df2['mail_domain'] = df2['contact_mail'].str.split('@').str[-1]
 
 match = df2[df2['mail_domain'] == df2['website']]
-
+len(match)
 
 # contact_mail 없는 플랫폼 수는?
 df2['contact_mail'].isna().sum()
